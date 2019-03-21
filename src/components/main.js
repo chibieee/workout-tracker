704ca1d1bdf3b27eaa3e6ad5c98f4fe850da5a26
Greg Garzon <gregneo21@gmail.com>
2019-03-21 16:10:24 -0400

    playing around with state

--- src/components/main.js
+++ src/components/main.js
@@ -3,11 +3,14 @@ import Options from './options/options';
 import View from './view/view';
 
 export default class Main extends Component {
+    state = {
+        view: 7
+    }
     render() {
         return (
             <div id="main__container">
                 <Options />
-                <View />
+                <View view={this.state.view} />
             </div>
         )
     }
--- src/components/view/view.js
+++ src/components/view/view.js
@@ -4,7 +4,7 @@ export default class View extends Component {
     render() {
         return (
             <div>
-                <div className="day-item"></div>
+                <div className="day-item">{this.props.view}</div>
                 <div className="day-item"></div>
                 <div className="day-item"></div>
                 <div className="day-item"></div>
