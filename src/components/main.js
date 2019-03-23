712263ce5272f9594d63e0b0ea5c935f160bc0df
chibieee <gregneo21@gmail.com>
2019-03-22 23:37:11 -0400

    set up new data structure for workouts

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import Options from './options/options';
+// import Options from './options/options';
 import View from './view/view';
 
 export default class Main extends Component {
-    state = {
-        view: 7
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewAmount: 1
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            viewAmount: event.target.value
+        })
+        console.log(this.state.viewAmount);
     }
     render() {
         return (
             <div id="main__container">
-                <Options />
-                <View view={this.state.view} />
+                <div className="view__container">
+                    <select onChange={this.handleChange}>
+                        <option value="1">Day</option>
+                        <option value="2">2 Day</option>
+                        <option value="7">Week</option>
+                        <option value="14">2 Week</option>
+                    </select>
+                </div>
+                <View view={this.state.viewAmount} />
             </div>
         )
     }
--- src/components/view/view.js
+++ src/components/view/view.js
@@ -1,11 +1,77 @@
 import React, { Component } from 'react'
 
+import './view.css';
+
 export default class View extends Component {
-    render() {
-        var items = [];
-        for (let i = 0; i < this.props.view; i++) {
-            items.push(<div className="day-item">hello {i}</div>)
+    testData = {
+        0 : {
+            name: 'Chest',
+            workouts: [
+                {
+                    name: 'Flat Barbell Bench Press',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Incline Barbell Bench Press',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Decline Barbell Bench Press',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Incline Dumbbell Fly',
+                    sets: 4,
+                    reps: 8,
+                },
+            ],
+            
+        },
+        1 : {
+            name: 'Back',
+            workouts: [
+                {
+                    name: 'Dumbbell Deadlift',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Kneeling One Arm Row',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Wide Row',
+                    sets: 4,
+                    reps: 8,
+                },
+                {
+                    name: 'Back Fly',
+                    sets: 4,
+                    reps: 8,
+                },
+            ]
         }
-        return items
+    }
+    render() {
+        const dayItems = Object.keys(this.testData).map((keyName, i) => (
+            <div className="day-item">
+                <h1>Day {i}</h1>
+                <h2>{this.testData[keyName].name}</h2>
+                <p>{this.testData[keyName].workouts.length}</p>
+                <div className="day-item-content">
+                    <h4>{this.testData[keyName].workouts[i].name}</h4>
+                    <p></p>
+                </div>
+            </div>
+        ));
+        return (
+            <div className="day__container">
+                {dayItems}
+            </div>
+        )
     }
 }
